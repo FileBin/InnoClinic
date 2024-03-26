c23a7d25ace4e5b096c6977b6f553606425eb1a5
filebin <alex.mousse9@gmail.com>
2024-03-26 17:43:29 +0100

    added DbContext

---
+++ Backend/OfficesAPI/Infrastructure/Database/OfficesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+using OfficesAPI.Domain.Models;
+
+namespace OfficesAPI.Infrastructure.Database;
+
+public class OfficesDbContext : DbContext {
+    public OfficesDbContext(DbContextOptions options) : base(options) {
+    }
+
+    public DbSet<Office> Movies => Set<Office>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Office>().ToCollection("offices");
+    }
+}
