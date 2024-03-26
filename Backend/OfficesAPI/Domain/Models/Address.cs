921a727e548f9a4ce757d49690489e42b5937e49
filebin <alex.mousse9@gmail.com>
2024-03-26 17:43:51 +0100

    added data annotaions to domain layer

--- Backend/OfficesAPI/Domain/Models/Address.cs
+++ Backend/OfficesAPI/Domain/Models/Address.cs
@@ -1,8 +1,15 @@
 namespace OfficesAPI.Domain.Models;
 
 public class Address {
+    [Column("city"), MaxLength(128)]
     public required string City { get; set; }
+
+    [Column("street"), MaxLength(128)]
     public required string Street { get; set; }
+
+    [Column("house_number"), MaxLength(16)]
     public required string HouseNumber { get; set; }
+    
+    [Column("office_number"), MaxLength(16)]
     public string? OfficeNumber { get; set; }
 }
--- Backend/OfficesAPI/Domain/Models/Office.cs
+++ Backend/OfficesAPI/Domain/Models/Office.cs
@@ -3,9 +3,18 @@
 namespace OfficesAPI.Domain.Models;
 
 public class Office {
+    [Column("id")]
     public ObjectId Id { get; set; }
+
+    [Column("address")]
     public required Address Address { get; set; }
+
+    [Column("registry_phone_number"), MaxLength(32)]
     public required string RegistryPhoneNumber { get; set; }
+    
+    [Column("photo_id")]
     public Guid? PhotoId { get; set; }
+
+    [Column("is_active")]
     public required bool IsActive { get; set; }
 }
