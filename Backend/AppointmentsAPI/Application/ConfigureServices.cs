8acf32f126d7ca888d8c22ebf4e935551efbc91d
filebin <alex.mousse9@gmail.com>
2024-05-23 14:17:00 +0200

    added time slot service

--- Backend/AppointmentsAPI/Application/ConfigureServices.cs
+++ Backend/AppointmentsAPI/Application/ConfigureServices.cs
@@ -1,5 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 using AppointmentsAPI.Application.Behaviors;
+using AppointmentsAPI.Application.Contracts.Services;
+using AppointmentsAPI.Application.Services;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +12,8 @@ namespace AppointmentsAPI.Application;
 public static class ConfigureServices {
     public static IServiceCollection AddApplication(this IServiceCollection services) {
         var assembly = typeof(ConfigureServices).Assembly;
+
+        services.AddScoped<ITimeSlotService, TimeSlotService>();
         
         services.AddValidatorsFromAssembly(assembly, includeInternalTypes: true);
 
---
+++ Backend/AppointmentsAPI/Application/Services/TimeSlotService.cs
@@ -0,0 +1,30 @@
+using AppointmentsAPI.Application.Contracts.Models.Requests;
+using AppointmentsAPI.Application.Contracts.Models.Responses;
+using AppointmentsAPI.Application.Contracts.Services;
+using AppointmentsAPI.Domain.Models;
+using InnoClinic.Shared.Domain.Abstractions;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppointmentsAPI.Application.Services;
+
+public class TimeSlotService(IRepository<Appointment> appointmentRepo) : ITimeSlotService {
+    public async Task<IEnumerable<TimeSlotResponse>> GetBusySlots(DateOnly date, CancellationToken cancellationToken = default) {
+        var appointments = await appointmentRepo.GetAll().Where(a => a.Date == date).OrderBy(a => a.BeginTime).ToListAsync(cancellationToken);
+        return appointments.Adapt<List<TimeSlotResponse>>();
+    }
+
+    public async Task<bool> VerifyTimeSlot(TimeSlotRequest timeSlot, CancellationToken cancellationToken = default) {
+        if (timeSlot.BeginTime >= timeSlot.EndTime) return false;
+
+        var timeSlots = await GetBusySlots(timeSlot.Date, cancellationToken);
+
+        foreach (var otherSlot in timeSlots) {
+            if (timeSlot.BeginTime > otherSlot.EndTime) continue;
+            if (otherSlot.BeginTime > timeSlot.EndTime) continue;
+
+            return false;
+        }
+        return true;
+    }
+}
