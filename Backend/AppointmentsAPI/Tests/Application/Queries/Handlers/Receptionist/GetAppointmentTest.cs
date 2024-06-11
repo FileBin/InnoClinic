345199ee99fed596aeff0e931c5253db1fa7b0fe
filebin <alex.mousse9@gmail.com>
2024-06-11 12:39:01 +0200

    added receptionist query tests

--- Backend/AppointmentsAPI/Tests/Application/Queries/Handlers/Receptionist/GetAppointmentTest.cs
+++ Backend/AppointmentsAPI/Tests/Application/Queries/Handlers/Receptionist/GetAppointmentTest.cs
@@ -3,6 +3,7 @@ using AppointmentsAPI.Application.Contracts.Models.Requests.Queries.Receptionist
 using AppointmentsAPI.Tests.Helpers;
 using Mapster;
 using AppointmentsAPI.Application.Contracts.Models.Responses;
+using InnoClinic.Shared.Exceptions.Models;
 
 namespace AppointmentsAPI.Tests.Application.Queries.Handlers.Receptionist;
 
@@ -25,5 +26,15 @@ public class GetAppointmentTest : TestBase {
 
         Assert.That(appointment, Is.Not.Null);
         Assert.That(appointment, Is.EqualTo(Objects.Appointment.Adapt<AppointmentResponse>()));
+    }
+
+    [Test]
+    [CancelAfter(3000)]
+    [TestCase("548c223e-5568-4d5f-bba8-0d7c47049b37")]
+    public void GetAppointmentThrowsNotFoundTest(string appointmentUUID, CancellationToken cancellationToken) {
+        Assert.ThrowsAsync<NotFoundException>(async ()
+        => await handler.Handle(new GetAppointmentQuery {
+            AppointmentId = Guid.Parse(appointmentUUID),
+        }, cancellationToken));
     }
 }
