90fb3509632aa2247f10597024e4abe41e799835
filebin <alex.mousse9@gmail.com>
2024-07-08 14:44:53 +0200

    fixed slider

---
+++ Frontend/WebApplication/src/app/widgets/loading/loading.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-loading',
+  standalone: true,
+  imports: [MatProgressSpinnerModule],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.scss'
+})
+export class LoadingComponent {
+  @Input() loaded = false;
+  @Input() diameter = 30;
+
+  get style() {
+    if (!this.loaded) { return 'display: none;' }
+    return ''
+  }
+}
--- Frontend/WebApplication/src/app/widgets/specializations-carousel/specializations-carousel.component.ts
+++ Frontend/WebApplication/src/app/widgets/specializations-carousel/specializations-carousel.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, afterNextRender } from '@angular/core';
+import { LoadingComponent } from '../loading/loading.component';
+import { Specialization } from '../../services/services-api-proxy.service';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-specializations-carousel',
   standalone: true,
-  imports: [],
+  imports: [LoadingComponent, NgbCarouselModule],
   templateUrl: './specializations-carousel.component.html',
   styleUrl: './specializations-carousel.component.scss'
 })
-export class SpecializationsCarouselComponent {
+export class SpecializationsCarouselComponent implements AfterViewInit {
+  loaded = false;
+  data: Specialization[] = [
+    {
+      id: "063d6e3b-a991-4ad8-b22e-0bc84b9d9df2",
+      isActive: true,
+      name: "Deontology",
+    },
+    {
+      id: "195b51e4-1d82-471d-a050-c7c0bfab81f4",
+      isActive: true,
+      name: "Healthcare",
+    },
+    {
+      id: "676356f8-fcb5-430b-932b-240f678feb4f",
+      isActive: true,
+      name: "Therapy",
+    }
+  ]
 
+  ngAfterViewInit(): void {
+    // DEMO loading
+    window.setTimeout(() => {
+      this.loaded = true;
+    }, 2000);
+  }
 }
